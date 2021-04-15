9456382d15faedc3e896d2e9aa1b192a3cc06823
Hyunggil Woo <hyunggilswoo@gmail.com>
2021-04-14 20:15:29 -0700

    this is a test

---
+++ StarFigures.java
@@ -0,0 +1,38 @@
+// Author Hyunggil Woo
+
+public class StarFigures {
+	public static void main(String[] args) {
+		
+		drawbox();
+		drawax();
+		drawline();
+		drawbox();
+		drawax();
+		drawbox();
+		drawline();
+		drawpole();
+		drawbox();
+		drawax();
+		
+	}
+	public static void drawbox () {
+		System.out.println ("*****");
+		System.out.println ("*****");
+		
+	}
+	public static void drawax () {
+		System.out.println (" * * ");
+		System.out.println ("  *  ");
+		System.out.println (" * * ");
+	}
+	public static void drawpole () {
+		System.out.println ("  *  ");
+		System.out.println ("  *  ");
+		System.out.println ("  *  ");
+		
+	}
+	public static void drawline () {
+		System.out.println ();
+		System.out.println ();
+	}
+}
