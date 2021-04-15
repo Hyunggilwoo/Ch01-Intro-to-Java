f350612f70ac0bf0d33031874bc4fb31438c696f
Hyunggil Woo <hyunggilswoo@gmail.com>
2021-04-14 20:17:00 -0700

    this is a test

---
+++ Spikey.java
@@ -0,0 +1,19 @@
+// Author Hyunggil Woo
+
+public class Spikey {
+	public static void main(String[] args) {
+		drawSpike();
+		drawCone();
+	}
+	public static void drawSpike () {
+			System.out.println ("  \\/");
+			System.out.println (" \\\\//");
+			System.out.println ("\\\\\\///");
+		}
+		public static void drawCone() {
+			System.out.println ("///\\\\\\");
+			System.out.println (" //\\\\");
+			System.out.println ("  /\\");
+		}
+		}
+
